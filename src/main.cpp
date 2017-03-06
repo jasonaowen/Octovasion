bc777bafe1175194700f920b5cd733f3fe72220f
Gwendolyn Weston <gwensfix@gmail.com>
2017-03-06 14:34:04 -0500

    adds our purple friend

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 #include <SDL2_image/SDL_image.h>
+#include <unistd.h>
 
 int main(int argc, const char * argv[]) {
     if(SDL_Init( SDL_INIT_VIDEO | SDL_INIT_TIMER ) < 0)
@@ -15,6 +16,27 @@ int main(int argc, const char * argv[]) {
     window = SDL_CreateWindow("GIP", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, winWidth, winHeight, SDL_WINDOW_SHOWN);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
+
+    const std::string filename = "../../purplefriend3x.png";
+    SDL_Texture *octofriend = IMG_LoadTexture(renderer, filename.c_str());
+    if ( octofriend == nullptr )
+    {
+        char cwd[1024];
+        if (getcwd(cwd, sizeof(cwd)) != NULL)
+            fprintf(stdout, "Current working dir: %s\n", cwd);
+
+        std::cerr << "Failed to load texture " << filename << " error : " << IMG_GetError() << std::endl;
+    }
+
+    int w; int h;
+    SDL_QueryTexture(octofriend, NULL, NULL, &w, &h);
+    SDL_Rect rect = {
+        (winWidth - w)/2,
+        winHeight - 2*h/3,
+        w,
+        h
+    };
+
     bool gameIsRunning = true;
     while(gameIsRunning)
     {
@@ -28,9 +50,8 @@ int main(int argc, const char * argv[]) {
             }
         }
         SDL_RenderClear(renderer);
-        SDL_Rect rect = {0, 0, winWidth, winHeight};
-        SDL_SetRenderDrawColor( renderer, 255, 0, 0, 255 );
-        SDL_RenderDrawRect(renderer, &rect);
+        SDL_RenderCopy(renderer, octofriend, NULL, &rect);
+
         SDL_RenderPresent(renderer);
     }
 
